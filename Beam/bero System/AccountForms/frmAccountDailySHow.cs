06a53041140b18f3c7dce3fdd2416d2ffa49277f
Abu Ehab <MyGlassBox@Hotmail.Com>
2015-01-17 18:49:15 +0200

    Account 1.0

--- Beam/bero System/AccountForms/frmAccountDailySHow.cs
+++ Beam/bero System/AccountForms/frmAccountDailySHow.cs
@@ -8,7 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Telerik.WinControls.UI;
-
+//================================
+using System.Threading;
+using DataLayer;
+using DataLayer.XAccountant;
 namespace bero_System.AccountForms
 {
     public partial class frmAccountDailySHow : RadForm
@@ -17,5 +20,36 @@ namespace bero_System.AccountForms
         {
             InitializeComponent();
         }
+
+        void PopulateGrid()
+        {
+            Operation.BeginOperation(this);
+            var q = AccountDailyCommand.GetAll ();
+            this.Invoke((MethodInvoker)delegate { DGVAccountsDaily.DataSource = q; });
+            Operation.EndOperation(this);
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void frmAccountDailySHow_Load(object sender, EventArgs e)
+        {
+
+            Thread th = new Thread(PopulateGrid);
+            th.Start();
+        }
+
+        private void RefreshBtn_Click(object sender, EventArgs e)
+        {
+            frmAccountDailySHow_Load( sender,  e);
+        }
+
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
