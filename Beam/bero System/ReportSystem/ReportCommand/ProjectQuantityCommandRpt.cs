43fe71c723daf32ef187a0c5702c51899a64025b
ainma007 <b-sword@outlook.com>
2015-01-28 23:44:54 +0200

    تصميم تقرير الكميات والمصروفات للمشروع

--- Beam/bero System/ReportSystem/ReportCommand/ProjectQuantityCommandRpt.cs
+++ Beam/bero System/ReportSystem/ReportCommand/ProjectQuantityCommandRpt.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using bero_System.ReportSystem.ReportOBj;
+using DataLayer.XProject;
+using Microsoft.Reporting.WinForms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +11,40 @@ namespace bero_System.ReportSystem.ReportCommand
 {
      public  class ProjectQuantityCommandRpt
     {
+         public ProjectQuantityCommandRpt() { }
 
+         public void rojectQuantityByProId(int ProId)
+        {
+            var q = ProjectQuantityCommand.GetByPojectProfileID(ProId);
+            ReportDataSource rs = new ReportDataSource();
+            List<ProjectQuantityObj> ls = new List<ProjectQuantityObj>();
+
+            foreach (var item in q)
+            {
+
+                ls.Add(new ProjectQuantityObj()
+                {////////////
+                    /// information Customer
+                   
+                    CustomerName=item.ProjectProfile.Customer.CustomerName,
+                    ProjectName=item.ProjectProfile.ProjectName,
+
+                   ItemName=item.BulidItem.ItemName,
+                    Qty=item.Qty
+
+                    
+                });
+            }
+            rs.Name = "DataSet1";
+            rs.Value = ls;
+            FrmReportViewer frm = new FrmReportViewer();
+            frm.reportViewer1.LocalReport.DataSources.Clear();
+            frm.reportViewer1.LocalReport.DataSources.Add(rs);
+            frm.reportViewer1.LocalReport.ReportEmbeddedResource = "bero_System.ReportSystem.ReportDesign.RptProjectQuantity.rdlc";
+            frm.ShowDialog();
+
+
+        }
 
     }
 }
