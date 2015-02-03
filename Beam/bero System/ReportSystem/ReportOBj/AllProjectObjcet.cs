1371d034f626702618944b9c3add1ae9d8432b38
abuehab <MyGlassBox@Hotmail.Com>
2015-02-03 09:58:44 +0200

    AllProjectsReport

---
+++ Beam/bero System/ReportSystem/ReportOBj/AllProjectObjcet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bero_System.ReportSystem.ReportOBj
+{
+   public  class AllProjectObjcet
+    {
+
+        public string ProjectName { get; set; }
+        public string  CustomerName { get; set; }       
+        public DateTime  CreationDate { get; set; }
+        public DateTime DeliverDate { get; set; }
+
+        public double FullAmount { get; set; }
+        public double Expensses { get; set; }
+        public double Selles { get; set; }
+        public double Installments { get; set; }
+
+        public string  Status { get; set; }
+
+    }
+}
